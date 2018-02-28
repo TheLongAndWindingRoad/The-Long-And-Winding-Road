00ff7388eddaa3801de9c51532a842d7b2047a11
DESKTOP-1IU2HTA\fatey <fate.yuki@icoud.com>
2018-03-01 03:41:53 +0900

    背景の処理を書きました

--- The Long And Winding Road/The Long And Winding Road/Main/Application/Scene/GameScene/ObjectManager/GameBackGround/GameBackGround.cpp
+++ The Long And Winding Road/The Long And Winding Road/Main/Application/Scene/GameScene/ObjectManager/GameBackGround/GameBackGround.cpp
@@ -1,8 +1,10 @@
-#include"GameBackGround.h"
-#include"DirectX9\TextureManager\TextureManager.h"
-#include"DirectX9\DirectGraphics\DirectGraphics.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include "GameBackGround.h"
+#include "DirectX9\TextureManager\TextureManager.h"
+#include "DirectX9\DirectGraphics\DirectGraphics.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 /* 画像の読み込みは統合ならばApp、一つ一つならばここで */
 
@@ -35,9 +37,17 @@ bool GameBackGround::Initalize()
 
 	for (int i = 0; i < 4; i++)
 	{
+		/* 初期化の時のx,y座標が全部同じでいいのか */
 		m_BackGround1[i] = BackGround[i];
+		m_BackGround2[i] = BackGround[i];
+		m_BackGround3[i] = BackGround[i];
 		m_Grass1[i] = Grass[i];
+		m_Grass2[i] = Grass[i];
+		m_Grass3[i] = Grass[i];
 	}
+
+	srand((unsigned)time(NULL));
+
 	return true;
 }
 
@@ -48,40 +58,109 @@ void GameBackGround::Finalize()
 
 void GameBackGround::Update()
 {
+	/* スクロールの処理 */
 	for (int i = 0; i < 4; i++)
 	{
 		m_BackGround1[i].y += 5;
-		m_BackGround1[i].y += 5;
-		m_BackGround2[i].y += 5;
 		m_BackGround2[i].y += 5;
 		m_BackGround3[i].y += 5;
-		m_BackGround3[i].y += 5;
-
-		m_Grass1[i].y += 5;
-		m_Grass1[i].y += 5;
-		m_Grass2[i].y += 5;
-		m_Grass2[i].y += 5;
-		m_Grass3[i].y += 5;
-		m_Grass3[i].y += 5;
 	}
 
+	/* 下まで行った時の処理 */
 	if (m_BackGround1[2].y > Height)
 	{
+		/* 余剰分の数字を入れている */
+		float tmp;
+
 		for (int i = 0; i < 4; i++)
 		{
-			// 問題がある.
-			// バルーンの時みたいにはみ出した分を余剰で計算
-			m_BackGround1[i].y -= Height * 3;
-			m_Grass1[i].y -= Height * 3;
+			/* 見てもらう */
+			m_BackGround1[i].y -= Height * 3 + modff(m_BackGround1[2].y, &tmp);
+		}
+		int r = rand() % 3 + 1;
 
-			// マップ初期化
-			// バックグラウンドと草に合わせて初期化
-
+		for (int i = 0; i < Map_Height; i++)
+		{
+			for (int j = 0; j < Map_Width; j++)
+			{
+				/* 乱数 */
+				switch (r)
+				{
+				case 1:
+					map1[i][j] = Initmap1[i][j];
+					break;
+				case 2:
+					map1[i][j] = Initmap2[i][j];
+					break;
+				case 3:
+					map1[i][j] = Initmap3[i][j];
+					break;
+				}
+			}
 		}
 	}
 
 	// 2,3でも同じ処理をする
+	if (m_BackGround2[2].y > Height)
+	{
+		float tmp;
 
+		for (int i = 0; i < 4; i++)
+		{
+			m_BackGround2[i].y -= Height * 3 + modff(m_BackGround2[2].y, &tmp);
+		}
+		int r = rand() % 3 + 1;
+
+		for (int i = 0; i < Map_Height; i++)
+		{
+			for (int j = 0; j < Map_Width; j++)
+			{
+				switch (r)
+				{
+				case 1:
+					map2[i][j] = Initmap1[i][j];
+					break;
+				case 2:
+					map2[i][j] = Initmap2[i][j];
+					break;
+				case 3:
+					map2[i][j] = Initmap3[i][j];
+					break;
+				}
+			}
+		}
+	}
+
+	if (m_BackGround3[2].y > Height)
+	{
+		float tmp;
+
+		for (int i = 0; i < 4; i++)
+		{
+			m_BackGround3[i].y -= Height * 3 + modff(m_BackGround3[2].y, &tmp);
+		}
+
+		int r = rand() % 3 + 1;
+
+		for (int i = 0; i < Map_Height; i++)
+		{
+			for (int j = 0; j < Map_Width; j++)
+			{
+				switch (r)
+				{
+				case 1:
+					map3[i][j] = Initmap1[i][j];
+					break;
+				case 2:
+					map3[i][j] = Initmap2[i][j];
+					break;
+				case 3:
+					map3[i][j] = Initmap3[i][j];
+					break;
+				}
+			}
+		}
+	}
 
 
 
@@ -93,17 +172,50 @@ void GameBackGround::Draw()
 	GrassDraw();
 }
 
-void GameBackGround::MapLoad(const char* mapdata)
+void GameBackGround::MapLoad(const char* mapdata,int number)
 {
 	FILE*  fp;
 	fopen_s(&fp, mapdata, "r");
 
+	int i = number;
 
 	for (int i = 0; i < Map_Height; i++)
 	{
 		for (int j = 0; j < Map_Width; j++)
 		{
-			fscanf_s(fp, "%d,", &map[i][j], _countof(map));
+			switch (i)
+			{
+			case 1:
+				fscanf_s(fp, "%d,", &Initmap1[i][j], _countof(map1));
+				for (int i = 0; i < Map_Height; i++)
+				{
+					for (int j = 0; j < Map_Width; j++)
+					{
+						map1[i][j] = Initmap1[i][j];
+					}
+				}
+				break;
+			case 2:
+				fscanf_s(fp, "%d,", &Initmap2[i][j], _countof(map2));
+				for (int i = 0; i < Map_Height; i++)
+				{
+					for (int j = 0; j < Map_Width; j++)
+					{
+						map2[i][j] = Initmap2[i][j];
+					}
+				}
+				break;
+			case 3:
+				fscanf_s(fp, "%d,", &Initmap3[i][j], _countof(map3));
+				for (int i = 0; i < Map_Height; i++)
+				{
+					for (int j = 0; j < Map_Width; j++)
+					{
+						map3[i][j] = Initmap3[i][j];
+					}
+				}
+				break;
+			}
 		}
 	}
 	fclose(fp);
@@ -120,7 +232,50 @@ void GameBackGround::BackGroundDraw()
 void GameBackGround::GrassDraw()
 {
 	TextureManager::GetInstance()->SetTexture(m_GrassTexture);
-	DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, m_Grass1, sizeof(CUSTOMVERTEX));
-	DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, m_Grass2, sizeof(CUSTOMVERTEX));
-	DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, m_Grass3, sizeof(CUSTOMVERTEX));
+
+	for (int i = 0; i < Map_Width; i++)
+	{
+		for (int j = 0; j < Map_Height; j++)
+		{
+			CUSTOMVERTEX maptip[4];
+			switch (map1[j][i])
+			{
+			case 0:
+				for (int k = 0; k < 4; k++)
+				{
+					maptip[k] = m_Grass1[k];
+					maptip[k].x = TipsizeWidth  * i;
+					maptip[k].y = TipsizeHeight * j;
+				}
+				DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, maptip, sizeof(CUSTOMVERTEX));
+				break;
+			}
+
+			switch (map2[j][i])
+			{
+			case 0:
+				for (int k = 0; k < 4; k++)
+				{
+					maptip[k] = m_Grass2[k];
+					maptip[k].x = TipsizeWidth  * i;
+					maptip[k].y = TipsizeHeight * j;
+				}
+				DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, maptip, sizeof(CUSTOMVERTEX));
+				break;
+			}
+			
+			switch (map2[j][i])
+			{
+			case 0:
+				for (int k = 0; k < 4; k++)
+				{
+					maptip[k] = m_Grass3[k];
+					maptip[k].x = TipsizeWidth  * i;
+					maptip[k].y = TipsizeHeight * j;
+				}
+				DirectGraphics::GetInstance()->GetDevice()->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, maptip, sizeof(CUSTOMVERTEX));
+				break;
+			}
+		}
+	}
 }
--- The Long And Winding Road/The Long And Winding Road/Main/Application/Scene/GameScene/ObjectManager/GameBackGround/GameBackGround.h
+++ The Long And Winding Road/The Long And Winding Road/Main/Application/Scene/GameScene/ObjectManager/GameBackGround/GameBackGround.h
@@ -14,11 +14,16 @@ public:
 	virtual void Finalize();
 	virtual void Update();
 	virtual void Draw();
-	void MapLoad(const char* mapdata);
 
+	/* csvƒtƒ@ƒCƒ‹‚ğ“Ç‚İ‚Ş‚½‚ß‚ÌŠÖ” */ 
+	void MapLoad(const char* mapdata,int number);
+
+	/* ”wŒi•`‰æ */
 	void BackGroundDraw();
+	/* ‘‚Ì•`‰æ */
 	void GrassDraw();
 
+	/* ‰æ‘œ”Ô†‚ğ•Ô‚·ŠÖ” */
 	int* GetBackGroundTexture() { return &m_BackGroundTexture; }
 	int* GetGrassTexture() { return &m_GrassTexture; }
 
@@ -31,15 +36,29 @@ private:
 		FLOAT	tu, tv;
 	};
 
-	static const int Map_Width = 11;
-	static const int Map_Height = 22;
-	const float Width = 550;
-	const float Height = 550;
+	/* Maptip‚Ì” */
+	static const int Map_Width = 13;
+	static const int Map_Height = 26;
+	/* ”wŒi‚Ì‘å‚«‚³ */
+	const float Width  = 650;
+	const float Height = 650;
+	/* Maptip‚Ì‘å‚«‚³ */
 	const float TipsizeWidth = 50;
 	const float TipsizeHeight = 25;
-	int map[Map_Height][Map_Width];
 
+	/* ”wŒi‚Ìƒf[ƒ^ */
+	int map1[Map_Height][Map_Width];
+	int map2[Map_Height][Map_Width];
+	int map3[Map_Height][Map_Width];
+
+	/* Maptip‚©‚ç“Ç‚İ‚ñ‚¾ƒf[ƒ^‚ğ‚±‚±‚ÉŠi”[‚·‚é */
+	int Initmap1[Map_Height][Map_Width];
+	int Initmap2[Map_Height][Map_Width];
+	int Initmap3[Map_Height][Map_Width];
+
+	/* ”wŒi‚Ì”Ô† */
 	int m_BackGroundTexture;
+	/* ‘‚Ì”Ô† */
 	int m_GrassTexture;
 
 	CUSTOMVERTEX m_BackGround1[4];
